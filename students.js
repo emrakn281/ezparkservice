f30d2c47ed68beb0d2d0cde60bba3d8eaae83355
EREN <EREN@DESKTOP-DRT5BB2>
2022-10-20 21:43:12 +0300

    make it better

--- students.js
+++ students.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('../data/db');
+const db = require('./db');
 
 app.get('/students', (req, res) => {
     db.query('SELECT * FROM students', (err, rows) => {
