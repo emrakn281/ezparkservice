b9de6eca2d3acc77565c55fa87c036b10f9fdcec
EREN <EREN@DESKTOP-DRT5BB2>
2022-10-20 21:34:08 +0300

    make it better

--- orders.js
+++ orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('../data/db');
+const db = require('./data/db');
 
 app.get('/orders', (req, res) => {
     db.query('SELECT * FROM orders', (err, rows) => {
