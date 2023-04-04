c1a08d98782555de2d440818ce67414210d7a084
Emre Akın <emr.akn.2001@gmail.com>
2023-04-04 15:01:59 +0300

    sa

--- routes/customers.js
+++ routes/customers.js
@@ -3,15 +3,27 @@ const app = express();
 const db = require('../data/db');
 
 app.get('/customers', (req, res) => {
-    db.query('SELECT * FROM deneme', (err, rows) => {
+    db.query('SELECT * FROM CUSTOMERS', (err, rows) => {
         res.send(rows);
     });
 });
 
 app.get('/addcustomer', (req, res) => {
-    productLine = req.query.productLine;
-    textDescription = req.query.textDescription;
-    db.query('insert into productlines (productLine,textDescription) values ('+productLine+', '+textDescription+')', (err, rows) => {
+    NAME = req.query.NAME;
+    SURNAME = req.query.SURNAME;
+    MAIL = req.query.MAIL;
+    PASSWORD = req.query.PASSWORD;
+    //şuanki zamanı alıyoruz ve mysql formatına çeviriyoruz
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    var CREATE_DATE = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+
+    db.query("insert into CUSTOMERS (NAME,SURNAME,MAIL,PASSWORD,CREATE_DATE) values ("+NAME+", "+SURNAME+","+MAIL+","+PASSWORD+",'"+CREATE_DATE+"' )", (err, rows) => {
     
         if (err) {
             res.send( {err, "message": "failed" });
